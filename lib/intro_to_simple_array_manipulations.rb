51a420ecd58d60d7545e32f327cae9852036cd32
Matt Long <mdlong34@gmail.com>
2020-05-28 21:37:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,14 +2,16 @@ def using_push(array, string)
   array.push(string)
 end
 
-using_unshift
+def using_unshift
+  
+end
 
-using_pop
+# using_pop
 
-pop_with_args
+# pop_with_args
 
-using_shift 
+# using_shift 
 
-shift_with_args
+# shift_with_args
 
 
